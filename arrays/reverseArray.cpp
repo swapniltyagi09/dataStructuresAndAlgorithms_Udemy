d24f2d7ae31fa1e659b272920270bd4665dd6c94
SWAPNIL TYAGI <swapniltyagianfield@gmail.com>
2025-08-22 01:33:24 +0530

    array reverse

---
+++ arrays/reverseArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void printArray(int arr[], int size){
+    for(int i = 0; i < size ; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+void swap(int &a , int &b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void reverseArray(int arr[],int size){
+    int i = 0, j = size-1;
+    while(i!=j) {
+         swap(arr[i],arr[j]);
+         i++;j--;
+    }
+}
+
+int main(){
+    int arr[] = {1,4,13,24,35,37,42,56,63};
+    int size = sizeof(arr)/sizeof(int);
+    printArray(arr,size);
+    reverseArray(arr,size); 
+    printArray(arr,size);
+    return 0;
+}
