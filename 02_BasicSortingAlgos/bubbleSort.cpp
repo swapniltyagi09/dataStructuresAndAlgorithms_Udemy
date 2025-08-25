c0754fb52c4f00f2ba522f7656c3fe1aa6329fae
SWAPNIL TYAGI <swapniltyagianfield@gmail.com>
2025-08-25 15:45:09 +0530

    mior changes

--- 02_BasicSortingAlgos/bubbleSort.cpp
+++ 02_BasicSortingAlgos/bubbleSort.cpp
@@ -8,18 +8,25 @@ void printVec(std::vector<int> vec){
 }
 
 void bubbleSort(std::vector<int> &vec){
+
+    
     for(int times = 1 ; times < vec.size() ; times++){
+        bool isSwapped = false;
         for(int i = 0 ; i < vec.size() - times ; i++)
-            if(vec[i] > vec[i+1])
+            if(vec[i] > vec[i+1]){
                 std::swap(vec[i],vec[i+1]);
-        
+                isSwapped = true;
+            }
+                
         printVec(vec);
+        if(!isSwapped)
+            break;    
     }
 }
 
 int main(){
 
-    std::vector<int> vec = {-2,3,4,-1,5,-12,6,1,3};
+    std::vector<int> vec = {1,2,3,4,5,6,7,8,9,10}; //{-2,3,4,-1,5,-12,6,1,3};
     printVec(vec);
 
     bubbleSort(vec);
